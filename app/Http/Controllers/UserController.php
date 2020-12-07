cb7c50710c648a1f81644b71238c9c742c093a9f
Onur Gultekin <onur@justmop.com>
2020-12-07 21:04:07 +0300

    API Authentication implemented

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function login(Request $request) {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || ($request->password !== $user->password)) {
+            return $response = [
+                'message' => 'We could not find the user with '.$request->email
+            ];
+        }
+        $token = $user->createToken('meditopia-token')->plainTextToken;
+
+        $response = [
+            'user'=> $user,
+            'token'=> $token
+        ];
+        return response($response, 201);
+    }
+}
